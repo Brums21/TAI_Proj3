26d0d32d04a05cea1f2e0f505a885cd1e6106fa6
Daniel <danielmartinsferreira@ua.pt>
2024-06-08 16:35:13 +0100

    Refactor compression methods and improve error handling
    Modified the generate_compression.py script to support multiple compression algorithms including gzip, bzip2, lzma, and zstd. Refactored separate functions for each compression method into a single function. Added comprehensive error checks for file and directory existence, and for checking valid compression methods.

--- generate_compression.py
+++ generate_compression.py
@@ -6,111 +6,131 @@ import zstandard as zstd
 import click
 import shutil
 
+COMPRESSORS = {
+    'gzip': gzip.open,
+    'bzip2': bz2.open,
+    'lzma': lzma.open,
+    'zstd': zstd.ZstdCompressor,
+}
+
+
 def clean_directory(directory_path):
     if os.path.exists(directory_path):
         shutil.rmtree(directory_path)
     os.makedirs(directory_path)
 
-def compress_gzip(input_file, output_file):
-    with open(input_file, 'rb') as f_in, gzip.open(output_file, 'wb') as f_out:
-        f_out.writelines(f_in)
 
-def compress_bzip2(input_file, output_file):
-    with open(input_file, 'rb') as f_in, bz2.open(output_file, 'wb') as f_out:
-        f_out.writelines(f_in)
+def compress_file(compressor, input_file, output_file):
+    try:
+        if compressor not in COMPRESSORS:
+            print(f"Invalid compressor: {compressor}. Please use a valid compressor.")
+            return
 
-def compress_lzma(input_file, output_file):
-    with open(input_file, 'rb') as f_in, lzma.open(output_file, 'wb') as f_out:
-        f_out.writelines(f_in)
+        if compressor == 'zstd':
+            with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
+                f_out.write(COMPRESSORS[compressor]().compress(f_in.read()))
+        else:
+            with open(input_file, 'rb') as f_in, COMPRESSORS[compressor](output_file, 'wb') as f_out:
+                f_out.writelines(f_in)
+    except Exception as e:
+        print(f"Error occurred while compressing file: {e}")
 
-def compress_zstd(input_file, output_file):
-    cctx = zstd.ZstdCompressor()
-    with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
-        f_out.write(cctx.compress(f_in.read()))
 
-def compress_files(gzip_flag, bzip2_flag, lzma_flag, zstd_flag):
+def get_outfile_path(compressed_dir, base_name, extension):
+    if not os.path.isdir(compressed_dir) or not base_name:
+        print(f"Invalid directory or base name: {compressed_dir}, {base_name}")
+        return None
+    return os.path.join(compressed_dir, f'{base_name}{extension}')
+
+
+def compress_files(compression_methods):
     signatures_dir = './song_signatures'
     compressed_dir = './compressed'
-    
+
+    if not os.path.isdir(signatures_dir) or not os.path.isdir(compressed_dir):
+        print("Invalid directory path / paths do not exist.")
+        return
+
     for sign_file in os.listdir(signatures_dir):
         input_path = os.path.join(signatures_dir, sign_file)
         base_name = os.path.splitext(sign_file)[0]
-
-        if gzip_flag:
-            compress_gzip(input_path, os.path.join(compressed_dir, f'{base_name}.gz'))
-        if bzip2_flag:
-            compress_bzip2(input_path, os.path.join(compressed_dir, f'{base_name}.bz2'))
-        if lzma_flag:
-            compress_lzma(input_path, os.path.join(compressed_dir, f'{base_name}.xz'))
-        if zstd_flag:
-            compress_zstd(input_path, os.path.join(compressed_dir, f'{base_name}.zst'))
-
+        for method in compression_methods:
+            compress_file(method, input_path, get_outfile_path(compressed_dir, base_name, '.' + method))
     print("Compression complete.")
 
+
 def concatenate_files(file1, file2, output_file):
+    if not os.path.isfile(file1) or not os.path.isfile(file2):
+        print("One or both input files do not exist.")
+        return
+
+    if os.path.exists(output_file):
+        print(f"Warning: {output_file} already exists and will be overwritten.")
     with open(output_file, 'wb') as f_out:
         for file in [file1, file2]:
             with open(file, 'rb') as f_in:
                 f_out.write(f_in.read())
 
+
 def get_file_size_in_bits(file_path):
+    if not os.path.isfile(file_path):
+        print(f"Invalid file path: {file_path}")
+        return None
     file_size_bytes = os.path.getsize(file_path)
     file_size_bits = file_size_bytes * 8
     return file_size_bits
 
-@click.command()
-@click.option('-c', '--compress', is_flag=True, help='Compress all songs')
-@click.option('-gzip', is_flag=True, help='Compress using gzip')
-@click.option('-bzip2', is_flag=True, help='Compress using bzip2')
-@click.option('-lzma', is_flag=True, help='Compress using lzma')
-@click.option('-zstd', is_flag=True, help='Compress using zstd')
-def main(compress, gzip, bzip2, lzma, zstd):
-    if compress:
-        compress_files(gzip, bzip2, lzma, zstd)
 
-    if not bzip2:
-        print("Currently only supporting bzip2 for NCD calculation.")
+@click.command()
+@click.option('-c', '--compress', is_flag=True, help='Compress all songs.')
+@click.option('-gzip', is_flag=True, help='Compress using gzip.')
+@click.option('-bzip2', is_flag=True, help='Compress using bzip2.')
+@click.option('-lzma', is_flag=True, help='Compress using lzma.')
+@click.option('-zstd', is_flag=True, help='Compress using zstd.')
+def main(compress, gzip, bzip2, lzma, zstd):
+    if not (compress and (gzip or bzip2 or lzma or zstd)):
+        print('Please provide valid compression option(s). Use --help for more info.')
+        return
+    compress_flags = {'gzip': gzip, 'bzip2': bzip2, 'lzma': lzma, 'zstd': zstd}
+    selected_methods = [method for method, flag in compress_flags.items() if flag]
+
+    if compress and selected_methods:
+        compress_files(selected_methods)
+    else:
+        print("No valid compression method specified.")
         return
 
-    # Ensure trim segment is compressed
     trim_segment_file = './trim/output_segment'
-    compressed_trim_segment_file = './trim/output_segment.bz2'
-    compress_bzip2(trim_segment_file, compressed_trim_segment_file)
-    
-    compressed_trim_segment = get_file_size_in_bits(compressed_trim_segment_file)
-    print(f"Compressed trim segment size in bits: {compressed_trim_segment}")
-
-    # Iterate over files in song_signatures and match with .bz2 files in compressed
-    song_signatures_dir = './song_signatures'
     compressed_dir = './compressed'
     files_ordered_by_bits = []
 
-    for signature_file in os.listdir(song_signatures_dir):
-        signature_file_path = os.path.join(song_signatures_dir, signature_file)
-        if os.path.isfile(signature_file_path):
-            
+    for method in selected_methods:
+        compressed_trim_segment_file = f'./trim/output_segment.{method}'
+        compress_file(method, trim_segment_file, compressed_trim_segment_file)
+        compressed_trim_segment = get_file_size_in_bits(compressed_trim_segment_file)
+
+        for signature_file in os.listdir('./song_signatures'):
+            signature_file_path = os.path.join('./song_signatures', signature_file)
             base_name = os.path.splitext(signature_file)[0]
-            
-            compressed_file_path = os.path.join(compressed_dir, f'{base_name}.bz2')
+            compressed_file_path = get_outfile_path(compressed_dir, base_name, '.' + method)
 
             if os.path.exists(compressed_file_path):
                 file_size_bits = get_file_size_in_bits(compressed_file_path)
-                
                 combined_file = './trim/temp_combined'
                 concatenate_files(signature_file_path, './trim/output_segment', combined_file)
-                
-                compress_bzip2('./trim/temp_combined', './trim/temp_combined.bz2')
-                combined_file_size_bits = get_file_size_in_bits('./trim/temp_combined.bz2')
-                
-                normalized_compressed_distance = (combined_file_size_bits - min(file_size_bits, compressed_trim_segment)) / max(file_size_bits, compressed_trim_segment)
-                files_ordered_by_bits.append({compressed_file_path: normalized_compressed_distance})
-                
-                os.remove('./trim/temp_combined.bz2')
-                os.remove('./trim/temp_combined')
-    
-    files_ordered_by_bits = sorted(files_ordered_by_bits, key=lambda x: list(x.values())[0])
-    for item in files_ordered_by_bits:
-        print(item)
+                compress_file(method, combined_file, f'./trim/temp_combined.{method}')
+                combined_file_size_bits = get_file_size_in_bits('./trim/temp_combined.' + method)
+                normalized_compressed_distance = (combined_file_size_bits - min(file_size_bits,
+                                                                                compressed_trim_segment)) / max(
+                    file_size_bits, compressed_trim_segment)
+                files_ordered_by_bits.append((compressed_file_path, normalized_compressed_distance))
+                os.remove('./trim/temp_combined.' + method)
+                os.remove(combined_file)
+
+    files_ordered_by_bits.sort(key=lambda item: item[1])
+    for file_path, distance in files_ordered_by_bits:
+        print(f"Path: {file_path}, Normalized Compressed Distance: {distance}")
+
 
 if __name__ == "__main__":
     main()
